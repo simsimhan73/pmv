fb43e29c96b9300e83979898d9b85cd872acc6fe
Verax <simsimhan73@gmail.com>
2023-09-05 14:50:10 +0900

    init

---
+++ src/lib.rs
@@ -0,0 +1,13 @@
+mod sender;
+mod receiver;
+mod finder;
+mod saver;
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        const RESULT : u16 = 4;
+        assert_eq!(RESULT, 4);
+    }
+}
